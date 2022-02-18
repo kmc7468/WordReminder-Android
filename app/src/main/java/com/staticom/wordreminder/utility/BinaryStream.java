06dfb0b1fb3419704da6547b6a6a4d2881bd5eeb
static <kmc7468@naver.com>
2022-02-19 01:33:48 +0900

    BinaryStream 클래스 추가

---
+++ app/src/main/java/com/staticom/wordreminder/utility/BinaryStream.java
@@ -0,0 +1,62 @@
+package com.staticom.wordreminder.utility;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class BinaryStream {
+
+    private final InputStream inputStream;
+    private final OutputStream outputStream;
+
+    public BinaryStream(InputStream inputStream) {
+        this.inputStream = inputStream;
+        this.outputStream = null;
+    }
+
+    public BinaryStream(OutputStream outputStream) {
+        this.inputStream = null;
+        this.outputStream = outputStream;
+    }
+
+    public byte[] read(int length) throws IOException {
+        final byte[] bytes = new byte[length];
+
+        inputStream.read(bytes);
+
+        return bytes;
+    }
+
+    public int readInt() throws IOException {
+        final byte[] bytes = read(4);
+
+        return bytes[0] & 0xFF | bytes[1] & (0xFF << 8) | bytes[2] & (0xFF << 16) | bytes[3] & (0xFF << 24);
+    }
+
+    public String readString() throws IOException {
+        final int length = readInt();
+        final byte[] bytes = read(length * 2);
+
+        return new String(bytes, StandardCharsets.UTF_16LE);
+    }
+
+    public void skip(int length) throws IOException {
+        inputStream.skip(length);
+    }
+
+    public void write(byte[] bytes) throws IOException {
+        outputStream.write(bytes);
+    }
+
+    public void writeInt(int value) throws IOException {
+        write(new byte[] { (byte)value, (byte)(value >>> 8), (byte)(value >>> 16), (byte)(value >>> 24) });
+    }
+
+    public void writeString(String value) throws IOException {
+        final byte[] bytes = value.getBytes(StandardCharsets.UTF_16LE);
+
+        writeInt(bytes.length / 2);
+        write(bytes);
+    }
+}
